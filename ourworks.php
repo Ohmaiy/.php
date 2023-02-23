80319d4c713cbbffee9578650a333a1040d44d78
Ohmaiy <117340711+Ohmaiy@users.noreply.github.com>
2023-02-24 01:07:05 +0800

    Add files via upload

---
+++ ourworks.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="en" >
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HWGL</title>
+
+    <link rel="icon" type="image/png" sizes="32x32" href="img/logo@2x.png">
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css"/>
+    <link rel="stylesheet" href="css/ourworks2.css">
+
+
+   </head>
+<body>
+        <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
+
+  <?php require_once('header.php') ?>
+
+        
+  
+  <section class="main-section">
+    <div class="main-content">
+        
+        <h1 class =  "fadeInUp" style = "animation-delay:1s">Our Works</h1>
+        <h2 class =  "fadeInUp" style = "animation-delay:1s">Please take a moment to explore our gallery of sample designs. Each piece has been carefully curated to showcase the breadth of our artistic capabilities.</h2>
+        <div class="top-content fadeInUp" style = "animation-delay:1s">
+            <button class="btn fil-cat active" href="" data-rel="all">All</button>
+            <button class="btn fil-cat active" data-rel="bags">Bags</button>
+            <button class="btn fil-cat active" data-rel="hats">Hats</button>
+            <button class="btn fil-cat active" data-rel="shirts">Shirts</button>
+            <button class="btn fil-cat active" data-rel="wallet">Wallet</button>
+            <button class="btn fil-cat active" data-rel="others">Others</button>
+
+        </div>
+
+        <div id = "portfolio" class = "fadeInUp" style = "animation-delay:1s">
+              
+            <!-- sHIRT -->
+              <div class="tile scale-anm SHIRT all">
+                <a href = "Shirtsproducts.php">
+                  <img src="img/shirts/1.jpg"   alt="" />
+                </a>
+              </div>
+
+              <div class="tile scale-anm SHIRT all">
+                <a href = "Shirtsproducts2.php">
+                  <img src="img/shirts/2.jpg" alt="" />
+                </a>
+              </div>
+
+              <div class="tile scale-anm SHIRT all">
+                <a href = "Shirtsproducts3.php">
+                  <img src="img/shirts/3.jpg" alt="" />
+                </a>
+              </div>
+
+              <div class="tile scale-anm SHIRT all">
+                <a href = "Shirtsproducts4.php">
+                 <img src="img/shirts/4.jpg" alt="" />
+                </a>
+              </div>
+
+              <div class="tile scale-anm SHIRT all">
+                <a href = "Shirtproducts5.php">
+                  <img src="img/shirts/OP1.png"   alt="" />
+                </a>
+              </div>
+
+              <div class="tile scale-anm SHIRTall">
+                <a href = "Shirtproducts6.php">
+                  <img src="img/shirts/unis1.png" alt="" />
+                </a>
+              </div>
+
+              <div class="tile scale-anm SHIRT all">
+                <a href = "Bagproducts7.php">
+                  <img src="img/shirts/PL1.png" alt="" />
+                </a>
+              </div>
+
+              <div class="tile scale-anm SHIRT all">
+                <a href = "Shirtproducts8.php">
+                 <img src="img/shirts/EDs1.png" alt="" />
+                </a>
+              </div>
+
+              <div class="tile scale-anm SHIRT all">
+                <a href = "Shirtproducts9.php">
+                  <img src="img/shirts/GYHU1.png"   alt="" />
+                </a>
+              </div>
+
+              <div class="tile scale-anm SHIRT all">
+                <a href = "Shirtproducts10.php">
+                  <img src="img/shirts/WM1.png" alt="" />
+                </a>
+              </div>
+
+              <div class="tile scale-anm SHIRTall">
+                <a href = "Shirtproducts11.php">
+                  <img src="img/shirts/Faith1.png" alt="" />
+                </a>
+              </div>
+
+              <div class="tile scale-anm SHIRT all">
+                <a href = "Shirtproducts12.php">
+                 <img src="img/shirts/AGU1.png" alt="" />
+                </a>
+              </div>
+
+              <div class="tile scale-anm SHIRT all">
+                <a href = "Shirtproducts13.php">
+                  <img src="img/shirts/WIU1.png"   alt="" />
+                </a>
+              </div>
+
+              <div class="tile scale-anm SHIRT all">
+                <a href = "Shirtproducts14.php">
+                  <img src="img/shirts/br1.png" alt="" />
+                </a>
+              </div>
+
+              <div class="tile scale-anm SHIRT all">
+                <a href = "Shirtproducts15.php">
+                  <img src="img/shirts/po1.png" alt="" />
+                </a>
+              </div>
+
+              <div class="tile scale-anm SHIRT all">
+                <a href = "Shirtproducts16.php">
+                 <img src="img/shirts/iron1.png" alt="" />
+                </a>
+              </div>
+
+          <!--************************************************-->
+              <div class="tile scale-anm shirts all">
+                <img src="img/shirts/shirts.jpg" alt="" />
+              </div>
+
+              <div class="tile scale-anm shirts all">
+                <img src="img/shirts/shirts.jpg" alt="" />
+              </div>
+              
+              <div class="tile scale-anm shirts all">
+                <img src="img/shirts/shirts.jpg" alt="" />
+              </div>
+              
+              <div class="tile scale-anm shirts all">
+                <img src="img/shirts/shirts.jpg" alt="" />
+              </div>
+
+              <div class="tile scale-anm shirts all">
+                <img src="img/shirts/shirts.jpg" alt="" />
+              </div>
+
+
+              <div class="tile scale-anm wallet all">
+                <img src="img/coin purse.jpg" alt="" />
+              </div>
+              
+              <div class="tile scale-anm wallet all">
+                <img src="img/coin purse.jpg" alt="" />
+              </div>
+              
+              <div class="tile scale-anm wallet all">
+                <img src="img/coin purse.jpg" alt="" />
+              </div>
+              
+              <div class="tile scale-anm wallet all">
+                <img src="img/coin purse.jpg" alt="" />
+              </div>
+              
+              <div class="tile scale-anm wallet all">
+                <img src="img/coin purse.jpg" alt="" />
+              </div>
+
+
+              <div class="tile scale-anm others all">
+                <img src = "img/calendar.jpg">
+              </div>
+
+              
+              <div class="tile scale-anm others all">
+                <img src = "img/calendar.jpg">
+              </div>
+              
+              <div class="tile scale-anm others all">
+                <img src = "img/calendar.jpg">
+              </div>
+              
+              <div class="tile scale-anm others all">
+                <img src = "img/calendar.jpg">
+              </div>
+              
+              <div class="tile scale-anm others all">
+                <img src = "img/calendar.jpg">
+              </div>
+
+              
+
+            
+        </div>
+
+
+        <?php require_once('footer.php') ?>
+
+        
+    </div>
+  </section>
+
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
+  <script src="home.js"></script>
+  <script>
+    $(function() {
+    var selectedClass = "";
+    $(".fil-cat").click(function(){
+    selectedClass = $(this).attr("data-rel");
+    $("#portfolio").fadeTo(100, 0.1);
+    $("#portfolio div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
+    setTimeout(function() {
+    $("."+selectedClass).fadeIn().addClass('scale-anm');
+    $("#portfolio").fadeTo(300, 1);
+    }, 300); 
+    
+});
+});
+  </script>
+
+</body>
+</html>
